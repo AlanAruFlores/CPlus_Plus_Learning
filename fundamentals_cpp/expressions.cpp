300e205a2ee5629abbf3a1f79de0cf6649c44a27
AlanAruFlores <alanaruquipa242@gmail.com>
2025-03-23 19:41:27 -0300

    expression in cpp

--- fundamentals_cpp/expressions.cpp
+++ fundamentals_cpp/expressions.cpp
@@ -20,8 +20,40 @@
 	 //Another way to short the results:
 	 printf("The result is %.1f \n",result);
 	
+	
+	/*
+		Math library examples
+	*/
+	
+	//sqrt(base) --> get the square
+	decimal num3  = sqrt(4);
+	printf("The square is %.1f \n", num3); 
 
-		
+	//pow(base,exp) 
+	decimal num4 = pow(4,2);
+	printf("The pow's result is %.1f \n", num4);
+	
+	//ceil(decimal) it's round to up your numbers
+	decimal num5 = ceil(4.789898); //5
+	printf("The result is %.1f \n", num5);
+	
+	//floor(decimal) it's round to down your number
+	decimal num6 = floor(4.72323); //4
+	printf("The result is %.1f \n",num6);
+	
+	//fmod(num1,num2) retrieve the rest's division	
+	decimal num7 = fmod(4,2); //0
+	printf("The rest of the division is %.1f\n",num7);
+	
+	//M_PI get the PI Number
+	decimal PI = M_PI;
+	printf("The PI number is %f\n",PI);
+	
+	//M_PI_2 get the PI Number divided by TWO
+	decimal PI_TWO  = M_PI_2;
+	printf("The PI / 2 number is %f\n",PI_TWO);
+	
+	
 	getch();
 	return 0;
  }
